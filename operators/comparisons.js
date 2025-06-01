aee071eac106c677a84e6bad8e3737486132b97c
Deyse Martellet <dmartellet@gmail.com>
2025-06-01 17:32:37 -0300

    01062025

---
+++ operators/comparisons.js
@@ -0,0 +1,10 @@
+function comparisons() {
+    console.log(5 > 2); //true
+    console.log(7 < 3); //false
+    console.log(6 === (3*2)); //true
+    console.log(4 !== 4); //false
+    console.log(527 !== 538); //true
+    console.log(6 != "6"); //false
+    console.log(6 !== "6") //true
+    console.log("cat" === "dog"); //false
+}
